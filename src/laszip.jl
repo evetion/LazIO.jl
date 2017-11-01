04113bd1df45521fb1226149db61896cc17c7206
Maarten Pronk <git@evetion.nl>
2017-11-01 14:47:49 +0100

    No sudo build.

--- src/laszip.jl
+++ src/laszip.jl
@@ -1,7 +1,7 @@
 # Julia wrapper for header: laszip_api.h
 # Automatically generated using Clang.jl wrap_c, version 0.0.0
 
-const laszip = Libdl.find_library(["laszip_api"])
+const laszip = Libdl.find_library(["liblaszip_api"])
 isempty(laszip) && error("could not find laszip library")
 
 function renameat(arg1::Cint, arg2, arg3::Cint, arg4)
