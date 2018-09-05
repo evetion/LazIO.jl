a8c1b1c25cf77276e8de0b948c7660e4aeeeb92d
Martijn Visser <mgvisser@gmail.com>
2018-09-05 13:52:01 +0200

    type stable iterator + test

--- test/testio.jl
+++ test/testio.jl
@@ -1,15 +1,21 @@
-push!(LOAD_PATH, "../")
-
 using FileIO
 using LazIO
 using LasIO
 using Test
 
-workdir = dirname(@__FILE__)
-
+workdir = @__DIR__
+lasio_testdir = joinpath(dirname(pathof(LasIO)), "..", "test")
 # source: http://www.liblas.org/samples/
-filename = "libLAS_1.2.laz" # point format 0
-testfile = File{format"LAZ_"}(joinpath(workdir, filename))
+fname_las = "libLAS_1.2.las" # point format 0
+fname = "libLAS_1.2.laz" # point format 0
+testfile_str = joinpath(lasio_testdir, fname)
+if !isfile(testfile_str)
+    @info "Creating testfile by running `laszip $fname` in $lasio_testdir"
+    cd(lasio_testdir) do
+        run(`laszip $fname_las`)
+    end
+end
+testfile = File{format"LAZ_"}(testfile_str)
 
 header, pointdata_all = load(testfile)
 # @show header
@@ -30,3 +36,15 @@ _, pointdata_colon = load(testfile, range=:)
 @test pointdata_all[1] == pointdata_colon[1] == pointdata_array[1]
 @test pointdata_276[1] == pointdata_colon[276] == pointdata_array[2]
 @test pointdata_all[end] == pointdata_colon[end] == pointdata_array[end]
+
+ds = LazIO.open(testfile_str)
+@test ds isa LazIO.LazDataset
+@test first(ds) isa LazIO.laszip_point
+@inferred first(ds)
+function it(ds)
+    ztot = Int32(0)
+    for p in ds
+        ztot += p.Z
+    end
+    ztot
+end
