0fb423b443862c1d30d3d220a14f4a1a201e16dc
Maarten Pronk <git@evetion.nl>
2020-05-25 18:57:16 +0200

    New build system.

--- src/LazIO.jl
+++ src/LazIO.jl
@@ -4,18 +4,10 @@ using Libdl
 using FileIO
 using LasIO
 using FixedPointNumbers
-
-# Load in `deps.jl`, complaining if it does not exist
-const depsjl_path = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("LazIO not installed properly, run Pkg.build(\"LazIO\"), restart Julia and try again")
-end
-include(depsjl_path)
+using LASzip_jll
 
 # Module initialization function
 function __init__()
-    # Always check your dependencies from `deps.jl`
-    check_deps()
     # temporary _ until LasIO defers this key
     add_format(format"LAZ_", (), ".laz", [:LazIO])
 end
