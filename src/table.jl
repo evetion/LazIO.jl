2474255bfeda7da4232174d6e4bfd79202938f7d
Maarten Pronk <evetion@users.noreply.github.com>
2021-02-26 09:28:16 +0000

    Accept kwargs in table interface that are passed to the Header constructor. (#31)

--- src/table.jl
+++ src/table.jl
@@ -13,10 +13,9 @@ const column_names = (fieldnames(LazPoint)..., :number_of_returns)
 function determine_offset(min_value, max_value, scale; threshold=10^7)
     s = round(Int64, ((min_value + max_value) / 2) / scale / threshold)
     s *= threshold * scale
-
     # Try to convert back and forth and check overflow
-    (muladd(round(Int32, (min_value - s) / scale), scale, s) > 0) == (min_value > 0) || error("Can't fit offset in this scale, try to coarsen it.")
-    (muladd(round(Int32, (max_value - s) / scale), scale, s) > 0) == (max_value > 0) || error("Can't fit offset in this scale, try to coarsen it.")
+    (muladd(round(Int32, (min_value - s) / scale), scale, s) > 0) == (min_value > 0) || error("Can't fit offset for min with this scale, try to coarsen it.")
+    (muladd(round(Int32, (max_value - s) / scale), scale, s) > 0) == (max_value > 0) || error("Can't fit offset for max with this scale, try to coarsen it.")
     s
 end
 
@@ -42,7 +41,7 @@ function Base.setproperty!(p::LazPoint, name, value, header)
     end
 end
 
-function write(fn::AbstractString, table, bbox; scale=0.01)
+function write(fn::AbstractString, table, bbox; scale=0.01, kwargs...)
 
     schema = Tables.schema(table)
     isnothing(schema) && error("A Schema is required")
@@ -50,7 +49,7 @@ function write(fn::AbstractString, table, bbox; scale=0.01)
 
     rows = Tables.rows(table)
 
-    header = LazHeader()
+    header = LazHeader(;kwargs...)
     header.x_offset = determine_offset(bbox.min_x, bbox.max_x, scale)
     header.y_offset = determine_offset(bbox.min_y, bbox.max_y, scale)
     header.z_offset = determine_offset(bbox.min_z, bbox.max_z, scale)
